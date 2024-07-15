49f516b7f763c6b107b83e8a711e317d247372e5
Cap2711 <chelsey.penton@keyin.com>
2024-07-15 10:20:08 -0230

    Dal
    implementing phase 8

---
+++ controllers/apiController.js
@@ -0,0 +1,34 @@
+const { getAllProducts, addProduct, updateProduct, deleteProduct } = require('../models/dal');
+
+// Get all products for API
+const getProducts = async (req, res) => {
+    const products = await getAllProducts();
+    res.json(products);
+};
+
+// Add a new product from API
+const addNewProduct = async (req, res) => {
+    const { name, price } = req.body;
+    const newProduct = await addProduct(name, price);
+    res.json(newProduct);
+};
+
+// Update a product from API
+const updateExistingProduct = async (req, res) => {
+    const { name, price } = req.body;
+    const updatedProduct = await updateProduct(req.params.id, name, price);
+    res.json(updatedProduct);
+};
+
+// Delete a product from API
+const deleteExistingProduct = async (req, res) => {
+    await deleteProduct(req.params.id);
+    res.sendStatus(204); 
+};
+
+module.exports = {
+    getProducts,
+    addNewProduct,
+    updateExistingProduct,
+    deleteExistingProduct,
+};
